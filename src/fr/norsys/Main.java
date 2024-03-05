6628f9cc19ef5fafae8ba5b29712de9b6fcb018a
Abdelwahed AB <ababdelwahed112@gmail.com>
2024-03-05 09:53:01 +0100

    fix(main) : fixing method

--- src/fr/norsys/Main.java
+++ src/fr/norsys/Main.java
@@ -13,7 +13,7 @@ public class Main {
      * Methode qui retourne le modulo de deux nombres
      */
     private static int getModulo(int agr1, int agr2) {
-        return agr1 / agr2;
+        return agr1 % agr2;
     }
     
     private static int multiplt(int a, int b) {
