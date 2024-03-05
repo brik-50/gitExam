cbe8f8399b2a0adf3203cd467b14f92e0630f3a8
imannel <afamanalafa2001@gmail.com>
2024-03-05 10:08:34 +0100

    ajouter une fonction de multiplication

--- src/fr/norsys/Main.java
+++ src/fr/norsys/Main.java
@@ -15,5 +15,9 @@ public class Main {
     private static int getModulo(int agr1, int agr2) {
         return agr1 % agr2;
     }
+    private static int getMultiplication(int agr1, int agr2) {
+        return agr1 * agr2;
+    }
+
 
 }
